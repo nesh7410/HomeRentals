45a111d9ccd482d7cc819c4ea5864bd7b0b516ee
Neelesh <nesh7410@gmail.com>
2024-06-28 20:20:58 +0530

    fix issue

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use('/api/user', userRouter);
