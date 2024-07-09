640057a197bd5ff22ff95bd30e71e80c3d40d4af
Neelesh <nesh7410@gmail.com>
2024-07-09 17:30:10 +0530

    added feature to search according to location

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -52,7 +52,7 @@ export const updateListing = async (req, res, next) => {
 
 export const getListing = async (req, res, next) => {
   try {
-    const listing = await Listing.findById(req.params.id);
+    const listing = await Listing.findById(req.params.id);  //params is from listing.route.js page
     if (!listing) {
       return next(errorHandler(404, 'Listing not found!'));
     }
@@ -65,39 +65,42 @@ export const getListing = async (req, res, next) => {
 export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
+    // console.log(req.query.limit);
     const startIndex = parseInt(req.query.startIndex) || 0;
-    let offer = req.query.offer;
 
+    let offer = req.query.offer;
     if (offer === undefined || offer === 'false') {
-      offer = { $in: [false, true] };
+      offer = { $in: [false, true] };        //$in operator to match documents where the  
+                                             // furnished field can be either false or true.
     }
 
     let furnished = req.query.furnished;
-
     if (furnished === undefined || furnished === 'false') {
       furnished = { $in: [false, true] };
     }
-
+    
     let parking = req.query.parking;
-
     if (parking === undefined || parking === 'false') {
       parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
-
     if (type === undefined || type === 'all') {
       type = { $in: ['sale', 'rent'] };
     }
 
     const searchTerm = req.query.searchTerm || '';
+    // console.log(req.query.searchTerm);
 
     const sort = req.query.sort || 'createdAt';
 
     const order = req.query.order || 'desc';
 
     const listings = await Listing.find({
-      name: { $regex: searchTerm, $options: 'i' },
+      $or: [
+        { name: { $regex: searchTerm, $options: 'i' } },
+        { address: { $regex: searchTerm, $options: 'i' } }
+      ],
       offer,
       furnished,
       parking,
